52f07d9ecdc9e18c09ca3071370aa82ff3cd7feb
NirangaNiluminda <niranganiluminda@gmail.com>
2025-01-07 19:08:37 +0530

    Update app.ts

--- server/app.ts
+++ server/app.ts
@@ -15,7 +15,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001','http://13.236.93.237:3001'], // Allow requests from localhost:3000
+    origin: ['http://localhost:3000', 'http://localhost:3001','http://13.210.73.56:3001'], // Allow requests from localhost:3000
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true, // If you're using cookies or authentication
     allowedHeaders: ['Content-Type', 'Authorization'],
