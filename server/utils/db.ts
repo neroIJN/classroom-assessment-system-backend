6746e6da15e4c7dd28c5a2b45399311ceb8d4113
Janith Niroshan <129001487+neroIJN@users.noreply.github.com>
2025-04-15 12:48:41 +0530

    update db.ts

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,17 +1,21 @@
 import mongoose from 'mongoose';
 require('dotenv').config();
 
-const dbUrl:string = process.env.DB_URL || '';
+const dbUrl: string = process.env.DB_URL || '';
+
+if (!dbUrl) {
+    throw new Error('DB_URL is not defined in the environment variables.');
+}
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(dbUrl).then((data:any) => {
-            console.log(`Database connected with ${data.connection.host}`)
-        })
-    } catch (error:any) {
-        console.log(error.message);
-        setTimeout(connectDB, 5000);
+        const connection = await mongoose.connect(dbUrl, {});
+        console.log(`Database connected with host: ${connection.connection.host}`);
+    } catch (error: any) {
+        console.error('Database connection error:', error.message);
+        console.log('Retrying in 5 seconds...');
+        setTimeout(connectDB, 5000); // Retry after 5 seconds
     }
-}
+};
 
 export default connectDB;
